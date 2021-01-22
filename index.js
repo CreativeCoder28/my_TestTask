e17fb1ebb732558a35d6e83d2e67d24473b3a50b
SakshiS28 <58689862+SakshiS28@users.noreply.github.com>
2021-01-22 02:41:21 +0100

    new pug files added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const path = require('path');
 
+//Init App
 const app =express();
+
+// Load view Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res){
-   res.send("hello World");
+   res.render('home');
 });
 
+// Dasboard Route
+app.get('/', function(req, res){
+  res.render('dashboard');
+  title: 'Dashboard'
+});
+
+
 app.listen(3000, function(){
-  console.log('post is 3000');
+  console.log('port is 3000');
 })
